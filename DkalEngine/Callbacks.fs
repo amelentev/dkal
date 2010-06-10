7e682ca03f09e82b9cadd6a2fb0dd594c35ec4bc
Michal Moskal <michal@moskal.me>
2010-06-10 10:55:02 -0700

    Add license (Apache 2.0) and license headers.

--- DkalEngine/Callbacks.fs
+++ DkalEngine/Callbacks.fs
@@ -1,4 +1,15 @@
-﻿namespace Microsoft.Research.DkalEngine
+﻿// *********************************************************
+//
+//    Copyright (c) Microsoft. All rights reserved.
+//    This code is licensed under the Apache License, Version 2.0.
+//    THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+//    ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+//    IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+//    PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+//
+// *********************************************************
+
+namespace Microsoft.Research.DkalEngine
 
 open Microsoft.Research.DkalEngine.Util
 open Microsoft.Research.DkalEngine.Ast
