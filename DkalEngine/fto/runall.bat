a9b8e419c1ae57325c644a946e16c15ef241be98
sritesh81 <devnull@localhost>
2010-06-23 14:03:59 -0700

    deleted unwanted folder

--- DkalEngine/fto/runall.bat
+++
@@ -1,4 +0,0 @@
-start ..\DkalController\bin\Debug\DkalController.exe kgc.dkal
-start ..\DkalController\bin\Debug\DkalController.exe site.dkal
-start ..\DkalController\bin\Debug\DkalController.exe physician.dkal
-start ..\DkalController\bin\Debug\DkalController.exe cro.dkal
