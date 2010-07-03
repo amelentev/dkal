3e35f0a6a17048128494659de1d9f4acb4d98c52
tumeh <devnull@localhost>
2010-07-03 09:18:07 -0700

    1. included readme files and substrate schema and data for clinical trials scenario 2. added comments in files as per guidelines

--- DkalEngine/UnitTests/DkalUnitTests/DkalUnitTests/ParsingCtxTest.cs
+++ DkalEngine/UnitTests/DkalUnitTests/DkalUnitTests/ParsingCtxTest.cs
@@ -1,4 +1,14 @@
-﻿using System;
+﻿// *********************************************************
+//
+//    Copyright (c) Microsoft. All rights reserved.
+//    This code is licensed under the Apache License, Version 2.0.
+//    THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+//    ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+//    IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+//    PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+//
+// *********************************************************
+using System;
 using System.Text;
 using System.Collections.Generic;
 using System.Linq;
