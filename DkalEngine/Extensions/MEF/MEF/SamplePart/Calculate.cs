a56be69ad85ac1f748bba041cd734ae4dbc559df
tumeh <devnull@localhost>
2010-07-16 00:10:13 -0700

    Creating new folder for Extension applications to support DkalEngine.

---
+++ DkalEngine/Extensions/MEF/MEF/SamplePart/Calculate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.Composition;
+using DkalLib;
+
+namespace SamplePart
+{
+    [Export(typeof(IDkalMessage))]
+    [ExportMetadata("DkalType", "CalculateDisplay")]
+    public class Calculate : IDkalMessage
+    {
+        #region IDkalMessage Members
+
+        public void InvokeMessage()
+        {
+            Console.WriteLine("SamplePart - calculate implementation called");
+        }
+
+        #endregion
+    }
+}
