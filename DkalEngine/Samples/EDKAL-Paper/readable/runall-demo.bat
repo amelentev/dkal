9589b0b0d3bacb0e847735c53ba20cc94106d11b
Michal Moskal <michal@moskal.me>
2010-10-14 19:00:04 -0700

    Add simple scripts to setup additional table for the controller example and package the demo.

---
+++ DkalEngine/Samples/EDKAL-Paper/readable/runall-demo.bat
@@ -0,0 +1,4 @@
+start DkalController.exe keymgr.dkal
+start DkalController.exe org1.dkal
+start DkalController.exe phys1.dkal
+start DkalController.exe site1.dkal
---
+++ DkalEngine/Samples/EDKAL-Paper/readable/runall.bat
@@ -0,0 +1,4 @@
+start ..\..\..\DkalController\bin\Debug\DkalController.exe keymgr.dkal
+start ..\..\..\DkalController\bin\Debug\DkalController.exe org1.dkal
+start ..\..\..\DkalController\bin\Debug\DkalController.exe phys1.dkal
+start ..\..\..\DkalController\bin\Debug\DkalController.exe site1.dkal
