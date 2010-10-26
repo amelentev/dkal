7a1249e017aeac1f2a630e6c270ce53ad7e9521f
Guido de Caso <gdecaso@gmail.com>
2010-10-26 13:26:32 -0700

    Now infons translation is also tested

--- GeneralPDP/Testing/Main.fs
+++ GeneralPDP/Testing/Main.fs
@@ -16,8 +16,9 @@ open System.IO
 
 module Main =
   let EXIT_CODE_XACML_DIFFERS = 1
-  let EXIT_CODE_DKAL_DIFFERS = 2
-  let EXIT_CODE_UNHANDLED_EXCEPTION = 3
+  let EXIT_CODE_DKAL_COMMRULES_DIFFERS = 2
+  let EXIT_CODE_DKAL_INFONS_DIFFERS = 3
+  let EXIT_CODE_UNHANDLED_EXCEPTION = 4
   
   let main () =
     let args = System.Environment.GetCommandLineArgs() |> Seq.toList |> List.tail
@@ -36,11 +37,11 @@ module Main =
       // parse response
       let rawContents = File.ReadAllText(args.[2])
       let response = parser.ParseResponse rawContents
-      printfn "%O" response
+      printfn "Expected response:\n%O" response
 
       // execute XACML engine and check response
       let response' = (pap policy) request
-      printfn "%O" response'
+      printfn "XACML PDP response:\n%O" response'
       if response.Decision <> response'.Decision then
         Environment.Exit(EXIT_CODE_XACML_DIFFERS)
 
@@ -53,28 +54,73 @@ module Main =
       let rspTranslator = DkalResponseTranslator()
 
       // create DKAL engine and install policy
+      let heardFromDkal = ref false
       let engine = DkalEngineInterface("engine", fun msg -> 
                                                    let _, _, response' = rspTranslator.TranslateResponse msg.message
-                                                   printfn "%O" response'
+                                                   printfn "DKAL CommRules response:\n%O" response'
+                                                   heardFromDkal.Value <- true
                                                    if response.Decision <> response'.Decision then
-                                                     Environment.Exit(EXIT_CODE_DKAL_DIFFERS))
+                                                     Environment.Exit(EXIT_CODE_DKAL_COMMRULES_DIFFERS))
       engine.SetDispatcher("first")
       for assertion in dPolicy.Assertions do
         engine.AddRule assertion
-      engine.Talk()
 
       // translate request to DKAL 
       let reqTranslator = XacmlRequestTranslator(pctx)
       let atts = attributesInPolicy policy
       let dRequest = reqTranslator.TranslateRequest "engine" request atts
 
-      // send the DKAL request to the DKAL engine
+      // send the DKAL request to the commrule DKAL engine
       engine.ReceiveMessage {source= pctx.LookupOrAddPrincipal "engine";
                              target= pctx.LookupOrAddPrincipal "engine";
                              message= dRequest;
                              proviso= Term.Empty}
+      engine.Talk()
       engine.CheckPoint()
       engine.Finish()
+      
+      // throw error if no response from dkal
+      if not heardFromDkal.Value then
+        Environment.Exit(EXIT_CODE_DKAL_COMMRULES_DIFFERS)
+
+      // create a second engine with single rule policy
+      let pctx, assertions = xacmlSingleCommRuleCtx "engine" "engine" "engine"
+      heardFromDkal.Value <- false
+      let engine = DkalEngineInterface("engine", fun msg -> 
+                                                   let _, _, response' = rspTranslator.TranslateResponse msg.message
+                                                   printfn "DKAL Infons response:\n%O" response'
+                                                   heardFromDkal.Value <- true
+                                                   if response.Decision <> response'.Decision then
+                                                     Environment.Exit(EXIT_CODE_DKAL_INFONS_DIFFERS))
+      for assertion in assertions do
+        engine.AddRule assertion
+
+      // translate the policy to DKAL as infons and send it to the engine
+      let pTraslator = XacmlPolicyTranslator("engine", "engine", pctx)
+      let infons = pTraslator.TranslatePolicyToInfons policy
+      for infon in infons do
+        let signature = App(pctx.LookupFunction("Ev.signedBy"), [Const(Principal(pctx.LookupOrAddPrincipal "engine")); infon; Const(Int(42))])
+        let certified = App(pctx.LookupFunction("certified"), [infon; signature]) 
+        engine.ReceiveMessage {source= pctx.LookupOrAddPrincipal "engine";
+                               target= pctx.LookupOrAddPrincipal "engine";
+                               message= certified;
+                               proviso= Term.Empty}
+
+      // send the request to the engine
+      let reqTranslator = XacmlRequestTranslator(pctx)
+      let atts = attributesInPolicy policy
+      let dRequest = reqTranslator.TranslateRequest "engine" request atts
+      engine.ReceiveMessage {source= pctx.LookupOrAddPrincipal "engine";
+                             target= pctx.LookupOrAddPrincipal "engine";
+                             message= dRequest;
+                             proviso= Term.Empty}
+      engine.Talk()
+      engine.CheckPoint()
+      engine.Finish()
+
+      // throw error if no response from dkal
+      if not heardFromDkal.Value then
+        Environment.Exit(EXIT_CODE_DKAL_INFONS_DIFFERS)
 
     with 
     | SyntaxError(p,s) -> 
