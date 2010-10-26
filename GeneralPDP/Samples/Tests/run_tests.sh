28d6330c4cee31b8566d24b1c9339914c5ef2e38
Guido de Caso <gdecaso@gmail.com>
2010-10-26 16:47:40 -0700

    better testing framewrk

--- GeneralPDP/Samples/Tests/run_tests.sh
+++ GeneralPDP/Samples/Tests/run_tests.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
+ok=0
+failed=0
 for i in $( ls *Request.xml ); do
 	item=${i:0:6}
 	./run_test.sh $item > /dev/null
 	res=$?
 	if [ "$res" = "0" ]; then
 		status="OK"
-	elif [ "$res" = "1" ]; then
-		status="Result from XACML engine differs from expected!!!!"
-	elif [ "$res" = "2" ]; then
-		status="Result from DKAL using COMM RULES differs from expected!!!!"
-	elif [ "$res" = "3" ]; then
-		status="Result from DKAL using INFONS differs from expected!!!!"
-	elif [ "$res" = "4" ]; then
-		status="Abnormal termination!"
+		ok=$((ok+1))
 	else
-		status="Unknown"
+		failed=$((failed+1))
+		if [ "$res" = "1" ]; then
+			status="Result from XACML engine differs from expected!!!!"
+		elif [ "$res" = "2" ]; then
+			status="Result from DKAL using COMM RULES differs from expected!!!!"
+		elif [ "$res" = "3" ]; then
+			status="Result from DKAL using INFONS differs from expected!!!!"
+		elif [ "$res" = "4" ]; then
+			status="Abnormal termination!"
+		else
+			status="Unknown"
+		fi
 	fi
+	
 	echo $item: $status
 	echo "------------------------------------------------------------------------------------------------"
 done
+echo "Done: $ok tests passed, $failed tests failed"
 
