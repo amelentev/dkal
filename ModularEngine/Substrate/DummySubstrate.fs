7cb67e8f44c8ffc8f0d85ee8a1471f98e78fda11
User <User@mnn>
2011-04-18 15:28:23 -0700

    DummySubstrate

---
+++ ModularEngine/Substrate/DummySubstrate.fs
@@ -0,0 +1,9 @@
+﻿namespace Microsoft.Research.Dkal.Substrate
+
+open Microsoft.Research.Dkal.Ast
+open Microsoft.Research.Dkal.Interfaces
+
+type DummySubstrate() = 
+  interface ISubstrate with
+    member this.Solve(query: MetaTerm) =
+      true
---
+++ ModularEngine/Substrate/SubstrateFactory.fs
@@ -0,0 +1,11 @@
+﻿namespace Microsoft.Research.Dkal.Substrate
+
+open Microsoft.Research.Dkal.Interfaces
+open Microsoft.Research.Dkal.Ast
+
+type SubstrateFactory() =
+  static member Substrate (substrate: MetaTerm) = 
+    new DummySubstrate()
+    // TODO ... "read" substrate metaterm and see if there is one connection already open and free
+    // in that case return that
+    // otherwise create a new one, return it and keep reference
