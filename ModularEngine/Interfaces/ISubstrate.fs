fb466c1a9d62448b4b5763686924cff66745a3d5
Artem Melentyev <artem@melentyev.me>
2011-04-20 19:58:27 -0700

    Modify ISubstrate Simple SubstrateDispatcher

--- ModularEngine/Interfaces/ISubstrate.fs
+++ ModularEngine/Interfaces/ISubstrate.fs
@@ -3,4 +3,7 @@
 open Microsoft.Research.Dkal.Ast
 
   type ISubstrate =
-    abstract Solve: query: MetaTerm -> Substitution seq
+    /// queries : boolean expression MetaTerms
+    /// substs  : seq of substitutions for queryes variables
+    /// returns seq of resolved substituions (more specialized than substs)
+    abstract member Solve :  queries : MetaTerm seq -> substs : Substitution seq -> Substitution seq
--- ModularEngine/Substrate/SubstrateDispatcher.fs
+++ ModularEngine/Substrate/SubstrateDispatcher.fs
@@ -3,8 +3,23 @@
 open Microsoft.Research.Dkal.Interfaces
 open Microsoft.Research.Dkal.Ast
 
+/// Dispatch queries to substrate across substrate implementations
 type SubstrateDispatcher() =
-  static member Solve (queries: MetaTerm list) (substs: Substitution list) = 
-    [Substitution.Id]
-    
-    // TODO ... 
+
+  /// return ISubstrate for subst label
+  static member GetSubstrate (subst : MetaTerm) : ISubstrate =
+    new DummySubstrate() :> ISubstrate
+    //TODO: match subst with
+    //    Sql(Const(SubstrateElemConstant(str))) -> new SqlSubstrate(str.ToString()) :> ISubstrate
+    //  | _ -> failwith("unrecorgnized substrate")
+
+  /// queries : asInfon queries
+  /// substs  : seq of substitutions to check
+  /// return  : seq of resolved substitutions (more specialized than substs)
+  static member Solve (queries: MetaTerm seq) (substs: Substitution seq) =
+    let groupedq = queries |> Seq.groupBy (function 
+        AsInfon(q, s) -> s
+      | _ -> failwith("Not asInfon"))
+    groupedq |> Seq.fold (fun res (substr, qs) ->
+      SubstrateDispatcher.GetSubstrate(substr).Solve qs res
+    ) substs
