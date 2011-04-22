d6f67904c4eecb225553478c311c8f204c510058
Lars Kuhtz <larsku@microsoft.com>
2011-04-21 18:30:00 -0700

    New version of ISubstrate

--- ModularEngine/Interfaces/ISubstrate.fs
+++ ModularEngine/Interfaces/ISubstrate.fs
@@ -1,6 +1,14 @@
 ﻿namespace Microsoft.Research.Dkal.Interfaces
 
+open System.Collections.Generic
+
 open Microsoft.Research.Dkal.Ast
 
   type ISubstrate =
-    abstract Solve: query: MetaTerm -> Substitution seq
+    abstract namespaces: HashSet<string>
+    abstract Solve: query: MetaTerm -> substitution: Substitution -> Substitution seq
+    abstract variables: query: MetaTerm -> Variable seq
+    abstract required: query: MetaTerm -> Variable seq
+    abstract unify: mt1: MetaTerm -> mt2: MetaTerm -> Substitution option
+    abstract unifyFrom: subst: Substitution -> mt1: MetaTerm -> mt2: MetaTerm -> Substitution option
+    abstract applySustitution: subst: Substitution -> mt: MetaTerm -> MetaTerm
