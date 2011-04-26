11cf17f1847ccfa3e1b0d0ab6621e5b3f3ac5152
Artem Melentyev <artem@melentyev.me>
2011-04-25 17:31:55 -0700

    new SqlSubstrate

---
+++ ModularEngine/SqlSubstrate/SqlConnector.fs
@@ -0,0 +1,84 @@
+﻿// *********************************************************
+//
+//    Copyright (c) Microsoft. All rights reserved.
+//    This code is licensed under the Apache License, Version 2.0.
+//    THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+//    ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+//    IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+//    PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+//
+// *********************************************************
+
+namespace Microsoft.Research.Dkal.SqlSubstrate
+
+open System
+open System.IO
+open System.Text
+open Microsoft.FSharp.Text
+open Microsoft.Research.Dkal.Ast
+open Microsoft.Research.Dkal.Ast.Infon
+open Microsoft.Research.Dkal.Ast.Tree
+open Microsoft.Research.Dkal.Interfaces
+open Microsoft.FSharp.Collections
+open System.Data
+open System.Data.SqlClient
+
+type SqlConnector(connStr) =
+  let conn = new SqlConnection(connStr)
+  let check() =
+    try
+      use comm = new SqlCommand("SELECT 1", conn)
+      comm.ExecuteScalar() |> ignore
+    with e ->
+      System.Console.WriteLine ("reopening database: " + e.Message)
+      conn.Open()
+  do
+    try
+      conn.Open()
+    with e -> printfn "%O" e
+      
+  member this.Close () =
+    conn.Close()
+
+  member this.ExecNonQuery s =
+    check()
+    let comm = new SqlCommand(s, conn)
+    comm.ExecuteNonQuery()
+  
+  member this.ExecReader s =
+    check()
+    let comm = new SqlCommand(s, conn)
+    comm.ExecuteReader()
+  
+  member this.GetCommand s =
+    new SqlCommand(s, conn)
+  
+  member this.ExecQuery (s:string, parms:seq<obj>, log) =
+    check()
+    if log then
+      System.Console.WriteLine ("execQ: {0} ::: {1}", s, parms |> Seq.mapi (fun i (o:obj) -> "@" + i.ToString() + ": " + o.ToString()) |> String.concat ", ")
+    let addParm (comm:SqlCommand) (idx:int) o =
+      comm.Parameters.AddWithValue ("p__" + idx.ToString(), o) |> ignore
+    seq {
+      use comm = new SqlCommand(s, conn)
+      do Seq.iteri (addParm comm) parms
+      use reader = comm.ExecuteReader()
+      while reader.Read() do
+        yield reader }
+      
+  member this.ReadVar (rd:Common.DbDataReader, var: IVar, idx:int) =
+    try
+      if var.Type = Type.Bool then
+        match rd.GetValue idx with
+          | :? bool as b -> SubstrateConstant b
+          | :? int as i -> SubstrateConstant (i <> 0)
+          | _ -> SubstrateConstant (rd.GetBoolean idx)
+      elif var.Type = Type.String then
+        SubstrateConstant (rd.GetString idx)
+      elif var.Type = Type.Principal then
+        SubstrateConstant (rd.GetString idx)
+      else
+        SubstrateConstant (rd.GetInt32 idx)
+    with :? InvalidCastException as e ->
+      System.Console.WriteLine ("cannot convert parm #{0}, to {1}, value: '{2}'", idx, var.Type.Name, rd.GetValue idx)
+      raise e
---
+++ ModularEngine/SqlSubstrate/SqlSubstrate.fs
@@ -0,0 +1,56 @@
+﻿namespace Microsoft.Research.Dkal.SqlSubstrate
+
+open System.Collections.Generic
+
+open Microsoft.Research.Dkal.Ast
+open Microsoft.Research.Dkal.Ast.Infon
+open Microsoft.Research.Dkal.Ast.Tree
+open Microsoft.Research.Dkal.Interfaces
+open Microsoft.Research.Dkal.Substrate
+open System.Data
+open System.Data.SqlClient
+open System.Text
+open System.Linq
+open System.Collections.Generic
+
+type SqlSubstrate(connStr : string) = 
+  let mutable nextId = 0
+  let conn = SqlConnector(connStr)
+
+  member private this.NextId () =
+    nextId <- nextId - 1
+    nextId
+
+  interface ISubstrate with
+    member this.Solve queries substs =
+      let queries = queries |> Seq.map (function
+        | :? DummySubstrateTerm as t -> t.Query
+        | _ as t -> failwithf "not DummySubstrateTerm: %A" t)
+      // translate all >2-ary functions functions to 2-ary
+      let rec normalize2 = function
+        | App(f, a1 :: a2 :: a3 :: tl) ->
+          let f' = {Name=f.Name; RetType=f.RetType; ArgsType=List.tail f.ArgsType}
+          let n = normalize2( App(f', a2::a3::tl) )
+          let f'' = {Name=f.Name; RetType=f.RetType; ArgsType=[f.ArgsType.Head; f.RetType]}
+          App(f'', [normalize2 a1; n])
+        | App(f, lst) ->
+          App(f, lst |> List.map normalize2)
+        | t -> t
+      // translate boolean (table.column) to (table.column=1)
+      let rec boolenize = function
+        | App(f, []) when f.RetType = Type.Bool && f.Name.Contains('.') ->
+          let eq = {Name="eq"; RetType=Type.Bool; ArgsType=[Type.Int32; Type.Int32]} : Function
+          App(eq, [App(f, []); Const(SubstrateConstant(1))])
+        | t -> t
+      let queries = queries |> Seq.map normalize2 |> Seq.map boolenize
+      let options = {Trace=9} : SqlCompiler.Options
+      let vars = new HashSet<IVar>()
+      queries |> Seq.iter (fun q -> vars.UnionWith(q.Vars))
+      let checkAssumptions (subst:ISubstitution) (assumptions: ITerm seq)=
+        let apply (t:ITerm) = t.Apply subst
+        let sqlExpr = SqlCompiler.compile options this.NextId (assumptions |> Seq.map apply)
+        SqlCompiler.execQuery (conn, options, sqlExpr, subst, Seq.toList(vars.AsEnumerable()))
+      substs |> Seq.collect (fun subst -> checkAssumptions subst queries)
+
+    member this.Namespaces = new HashSet<string>()
+    member this.RequiredVars (query: ISubstrateTerm) = []
