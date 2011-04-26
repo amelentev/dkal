dca853d75ae52475d34a17d664e3ee28dc7a1edf
Guido de Caso <gdecaso@gmail.com>
2011-04-25 17:08:20 -0700

    Missing file

---
+++ ModularEngine/Ast/ActivePatterns.fs
@@ -0,0 +1,13 @@
+﻿[<AutoOpen>]
+module Microsoft.Research.Dkal.Ast.ActivePatterns
+
+  open Microsoft.Research.Dkal.Interfaces
+
+  // Type patterns
+  let (|Infon|_|) t = if t = Type.Infon then Some () else None
+  let (|Principal|_|) t = if t = Type.Principal then Some () else None
+  let (|Action|_|) t = if t = Type.Action then Some () else None
+  let (|Rule|_|) t = if t = Type.Rule then Some () else None
+  let (|Substrate|_|) (t: IType) =  match t with
+                                    | :? Type.Substrate as t -> Some t.Type
+                                    | _ -> None
