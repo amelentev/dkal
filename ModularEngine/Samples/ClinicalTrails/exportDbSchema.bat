79b10ad6cbcd507fc4fa10973151bcf8aad9e4ee
Artem Melentyev <artem@melentyev.me>
2011-04-26 19:01:59 -0700

    Clinical Trials Sample for ModularEngine. WIP

---
+++ ModularEngine/Samples/ClinicalTrails/exportDbSchema.bat
@@ -0,0 +1,1 @@
+"C:\Program Files\Microsoft SDKs\Windows\v7.0A\bin\NETFX 4.0 Tools\SqlMetal.exe" /conn:"Server=localhost,1136;Database=edkaldemo;User ID=dkal;Password=dkal;Trusted_Connection=True;Encrypt=False;" /dbml:schema.dbml
---
+++ ModularEngine/Samples/ClinicalTrails/run.bat
@@ -0,0 +1,2 @@
+start ..\..\Main\bin\Debug\Main.exe org1.rt org1.dkal simple
+start ..\..\Main\bin\Debug\Main.exe site1.rt site1.dkal simple
