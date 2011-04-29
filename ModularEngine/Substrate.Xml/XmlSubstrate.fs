2d122815135854f27eb019230d5ef659dd6edd99
Guido de Caso <gdecaso@gmail.com>
2011-04-29 16:16:53 -0700

    First skeleton of XmlSubstrate

---
+++ ModularEngine/Substrate.Xml/XmlSubstrate.fs
@@ -0,0 +1,18 @@
+﻿namespace Microsoft.Research.Dkal.Substrate.Xml
+
+open Microsoft.Research.Dkal.Interfaces
+
+open System.Collections.Generic
+open System.Xml
+
+type XmlSubstrate(xmlFile: string, xsdFile: string, namespaces: string list) = 
+  
+  interface ISubstrate with
+    member xs.RequiredVars st = 
+      st.Vars
+
+    member xs.Solve queries substs =
+      failwith "unimplemented"
+
+    member xs.Namespaces = new HashSet<_>(namespaces)
+
---
+++ ModularEngine/Substrate.Xml/XmlSubstrateTerm.fs
@@ -0,0 +1,22 @@
+﻿namespace Microsoft.Research.Dkal.Substrate.Xml
+
+open Microsoft.Research.Dkal.Interfaces
+open Microsoft.Research.Dkal.Ast
+
+type XmlSubstrateTerm(ns: string, xpath: string, vars: IVar list, outputVar: IVar) =
+
+  let mutable subst = Substitution.Id
+
+  interface ISubstrateTerm with
+    
+    member x.Type = Type.Boolean
+    member x.Vars = vars
+    member x.Apply subst' =
+      subst <- subst.ComposeWith subst'
+      x :> ITerm
+    member x.Normalize() = x :> ITerm
+    member x.UnifyFrom _ _ = None
+    member x.Unify _ = None
+
+    member x.Namespace = ns
+
