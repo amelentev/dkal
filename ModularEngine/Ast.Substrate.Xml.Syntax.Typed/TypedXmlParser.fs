54c49dd76de7fe6ae3dff4ef408a8147ac56b88b
Guido de Caso <gdecaso@gmail.com>
2011-05-02 15:58:22 -0700

    Typed XML parser

---
+++ ModularEngine/Ast.Substrate.Xml.Syntax.Typed/TypedXmlParser.fs
@@ -0,0 +1,33 @@
+﻿namespace Microsoft.Research.Dkal.Ast.Substrate.Xml.Syntax.Typed
+
+open System.IO
+open Microsoft.FSharp.Text.Lexing
+
+open Microsoft.Research.Dkal.Ast
+open Microsoft.Research.Dkal.Interfaces
+open Microsoft.Research.Dkal.Substrate
+open Microsoft.Research.Dkal.Substrate.Xml
+
+open System.Collections.Generic
+
+/// The TypedParser parses from the typed concrete syntax, which uses explicit
+/// typed variables
+type TypedXmlParser() = 
+
+  let lexbuff s = LexBuffer<char>.FromString(s)
+
+  interface ISubstrateParser with
+
+    member sp.SetParsingContext (context: IParsingContext) = 
+      ()
+
+    member sp.SetNamespace (ns: string) = 
+      Parser.Namespace <- ns
+
+    member sp.SetSubstrate (substrate: ISubstrate) =
+      ()
+
+    member sp.ParseTerm s = 
+      let smt = Parser.XmlSubstrateTerm Lexer.tokenize (lexbuff s)
+      smt :> ISubstrateTerm
+
---
+++ ModularEngine/Ast.Substrate.Xml.Syntax.Typed/TypedXmlPrettyPrinter.fs
@@ -0,0 +1,32 @@
+﻿namespace Microsoft.Research.Dkal.Ast.Substrate.Xml.Syntax.Simple
+
+open Microsoft.Research.Dkal.Interfaces
+open Microsoft.Research.Dkal.Utils.PrettyPrinting
+open Microsoft.Research.Dkal.Substrate
+open Microsoft.Research.Dkal.Substrate.Xml
+
+open System.Collections.Generic
+
+/// The TypedXmlPrettyPrinter prints substrate elements into the typed concrete syntax
+type TypedXmlPrettyPrinter() =
+    
+  interface ISubstratePrettyPrinter with
+    member spp.PrintTerm t =
+      match t with
+      | :? XmlSubstrateTerm as t ->
+        PrettyPrinter.PrettyPrint <| spp.TokenizeTerm t
+      | _ -> failwith "Expecting DummySubstrateTerm when printing SimpleSqlSyntax"
+
+  member private spp.PrintType (t: IType) = t.FullName
+
+  member private spp.PrintVar (v: IVar) = v.Name + ":" + spp.PrintType v.Type
+
+  member private spp.TokenizeTerm (t: XmlSubstrateTerm) =
+    let outputVars = String.concat "," 
+                      <| Seq.map (fun (kv: KeyValuePair<string,IVar>) -> 
+                                    match kv.Key with
+                                    | "" -> spp.PrintVar kv.Value
+                                    | att -> spp.PrintVar kv.Value + "<->\"" + att + "\"") t.OutputVars
+    let vars = String.concat "," 
+                <| List.map (fun (v: IVar) -> spp.PrintVar v) t.Vars
+    [ TextToken <| "\"" + t.XPath + "\"|" + outputVars + "|" + vars ]
