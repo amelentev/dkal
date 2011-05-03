71d7a68e9758870e849d12725588206aa7266aba
Guido de Caso <gdecaso@gmail.com>
2011-05-03 12:20:58 -0700

    Term for SQL substrate modification

---
+++ ModularEngine/Substrate.Sql/SqlSubstrateModifyTerm.fs
@@ -0,0 +1,11 @@
+﻿namespace Microsoft.Research.Dkal.Substrate.Sql
+
+open System.Collections.Generic
+
+open Microsoft.Research.Dkal.Interfaces
+
+type SqlSubstrateModifyTerm(ns: string, query: ITerm, colsMapping) = 
+
+  interface ISubstrateUpdateTerm with
+    ()
+    
