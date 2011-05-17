6577e2c5bfbee36505e0a00d85dd8df4fd57799b
Guido de Caso <gdecaso@gmail.com>
2011-05-16 17:31:50 -0700

    Working version of the clinical trials with new schema (no principal integer ids)

--- ModularEngine/Samples/ClinicalTrials/run.bat
+++ ModularEngine/Samples/ClinicalTrials/run.bat
@@ -1,4 +1,4 @@
 start ..\..\Main\bin\Debug\Main.exe org1.rt org1.dkal simple
 start ..\..\Main\bin\Debug\Main.exe site1.rt site1.dkal simple
 start ..\..\Main\bin\Debug\Main.exe phys1.rt phys1.dkal simple
-start ..\..\Main\bin\Debug\Main.exe keyMgr.rt keyMgrXml.dkal simple
+start ..\..\Main\bin\Debug\Main.exe keyMgr.rt keyMgr.dkal simple
