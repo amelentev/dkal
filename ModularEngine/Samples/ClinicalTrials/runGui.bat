ec0be1f1578d5d040f2d6633638de733edf1acda
Guido de Caso <gdecaso@gmail.com>
2011-05-19 14:13:53 -0700

    To use the GUI

---
+++ ModularEngine/Samples/ClinicalTrials/runGui.bat
@@ -0,0 +1,4 @@
+start ..\..\MainGui\bin\Debug\MainGui.exe org1.rt org1.dkal
+start ..\..\MainGui\bin\Debug\MainGui.exe site1.rt site1.dkal
+start ..\..\MainGui\bin\Debug\MainGui.exe phys1.rt phys1.dkal
+start ..\..\MainGui\bin\Debug\MainGui.exe keyMgr.rt keyMgr.dkal
