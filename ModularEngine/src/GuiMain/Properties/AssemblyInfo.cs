2e3305d4087c82ed016102e9f22a05cd23a9dbf9
Guido de Caso <gdecaso@gmail.com>
2011-06-06 17:34:44 -0700

    Made target platform consistently ´any cpu´

--- ModularEngine/src/GuiMain/Properties/AssemblyInfo.cs
+++ ModularEngine/src/GuiMain/Properties/AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Microsoft Research DKAL GUI")]
+[assembly: AssemblyTitle("DKAL Graphical User Interface")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
@@ -43,5 +43,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.364")]
+[assembly: AssemblyFileVersion("1.0.0.364")]
