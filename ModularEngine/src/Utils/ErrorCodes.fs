079e062a2750c8a21b4b22c39204ad3bce1d7613
Guido de Caso <gdecaso@gmail.com>
2011-06-24 14:24:59 -0300

    Changed error reporting to match rise4fun requirements

---
+++ ModularEngine/src/Utils/ErrorCodes.fs
@@ -0,0 +1,14 @@
+﻿// *********************************************************
+//
+//    Copyright (c) Microsoft. All rights reserved.
+//    This code is licensed under the Apache License, Version 2.0.
+//    THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+//    ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+//    IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+//    PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+//
+// *********************************************************
+
+module Microsoft.Research.Dkal.Utils.ErrorCodes
+
+  let errorParsing = "D001"
