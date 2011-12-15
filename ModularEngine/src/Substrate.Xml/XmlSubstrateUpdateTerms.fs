b17849ea4a89c44da1913e38b872b33ac74f221a
Artem Melentyev <artem@melentyev.me>
2011-12-15 19:01:15 +0600

    Mutable XML Substrate (+insert, update, delete operations). example in MultiMain/xmlupdate.mdkal

---
+++ ModularEngine/src/Substrate.Xml/XmlSubstrateUpdateTerms.fs
@@ -0,0 +1,90 @@
+﻿// *********************************************************
+//
+//    Copyright (c) Microsoft. All rights reserved.
+//    This code is licensed under the Apache License, Version 2.0.
+//    THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+//    ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+//    IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+//    PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+//
+// *********************************************************
+namespace Microsoft.Research.Dkal.Substrate.Xml
+
+open System.Collections.Generic
+open Microsoft.Research.Dkal.Interfaces
+open Microsoft.Research.Dkal.Ast
+
+/// base for XmlSubstrateUpdate terms
+[<AbstractClass>]
+type AXmlSubstrateUpdateTerm(ns: string, xpath: string, vars: IVar list) =
+  member x.XPath = xpath
+  interface ISubstrateUpdateTerm with
+    member x.Namespace = ns
+  interface ITerm with
+    member x.Type = Type.SubstrateUpdate
+    member x.Unify t = (x:>ITerm).UnifyFrom Substitution.Id t
+    member x.Vars = vars
+    member x.Apply subst = x :> ITerm
+    member x.BoundVars = []
+    member x.Normalize() = x :> ITerm
+    member x.UnifyFrom s t = None
+  override x.Equals y =
+    match y with
+    | :? AXmlSubstrateUpdateTerm as y ->
+      ns = (y :> ISubstrateUpdateTerm).Namespace
+        && xpath = y.XPath
+        && HashSet(vars).SetEquals((y:>ITerm).Vars)
+    | _ -> false
+  override x.GetHashCode() =
+    (ns, xpath, vars).GetHashCode()
+
+/// Modify xml nodes
+///  for each (attr,value) in attributes dict
+///   if attr<>"" & value<>None then modify attribute attr to value
+///   if attr<>"" & value=None then remove attribute attr from xml node
+///   if attr="" & value<>None then rename node to value
+///   if attr="" & value=None then remove node
+type XmlSubstrateModifyTerm(ns: string, xpath: string, vars: IVar list,  attrs: Map<string, ITerm option>) =
+  inherit AXmlSubstrateUpdateTerm(ns, xpath, vars)
+  member x.Attrs = attrs
+  interface ITerm with
+    override x.Vars =
+      let varsInAttrs = attrs |> Map.toList |> Seq.map snd |> Seq.choose (fun x -> x) |> Seq.collect (fun x -> x.Vars)
+      HashSet(Seq.concat [seq vars; varsInAttrs]) |> Seq.toList
+    override x.Apply subst =
+      let xpath, vars = XSUtil.xpathApply xpath vars subst
+      XmlSubstrateModifyTerm(ns, xpath, vars,
+        (attrs |> Map.map (fun k -> Option.map (XSUtil.termApply x subst))) ) :> ITerm
+  override x.Equals y =
+    match y with
+    | :? XmlSubstrateModifyTerm as y -> base.Equals y && attrs = y.Attrs
+    | _ -> false
+  override x.GetHashCode() = (base.GetHashCode(), attrs).GetHashCode()
+  override x.ToString() =
+    if (attrs |> Map.exists (fun attr term -> attr="" && term=None)) then
+      sprintf "{| \"%s\" | delete \"%s\" |}" ns xpath
+    else
+      sprintf "{| \"%s\" | update \"%s\" | %s |}" ns xpath (XSUtil.mappingToStringO attrs)
+
+/// Insert xml node with attributes.
+///   NodeName = attrs.[""]
+type XmlSubstrateInsertTerm(ns: string,  xpath: string, vars: IVar list, attrs: Map<string, ITerm>) =
+  inherit AXmlSubstrateUpdateTerm(ns, xpath, vars)
+  member x.NodeName = attrs.[""]
+  member x.Attrs = attrs
+  interface ITerm with
+    override x.Vars =
+      let varsInAttrs = attrs |> Map.toSeq |> Seq.map snd |> Seq.collect (fun x -> x.Vars)
+      HashSet(Seq.concat [seq vars; varsInAttrs]) |> Seq.toList
+    override x.Apply subst =
+      let xpath, vars = XSUtil.xpathApply xpath vars subst
+      XmlSubstrateInsertTerm(ns, xpath, vars,
+        (attrs |> Map.map (fun k v -> v.Apply subst)) ) :> ITerm
+  override x.Equals y = 
+    match y with
+    | :? XmlSubstrateInsertTerm as y ->
+      base.Equals y && attrs = y.Attrs
+    | _ -> false
+  override x.GetHashCode() = (base.GetHashCode(), attrs).GetHashCode()
+  override x.ToString() =
+    sprintf "{| \"%s\" | insert \"%s\" | %s |}" ns xpath (XSUtil.mappingToString attrs)
---
+++ ModularEngine/src/Substrate.Xml/XmlTermsTest.fsx
@@ -0,0 +1,108 @@
+﻿#I @".\bin\debug\"
+#r "Ast.dll"
+#r "Interfaces.dll"
+#r "Substrate.Xml.dll"
+#r "FSharp.PowerPack.dll"
+#r "System.Xml.Linq.dll"
+
+open System.Xml
+open System.Xml.Linq
+open System.Xml.XPath
+open System.Linq
+open System.IO
+open System.Collections.Generic
+open Microsoft.Research.Dkal.Substrate.Xml
+open Microsoft.Research.Dkal.Ast
+open Microsoft.Research.Dkal.Interfaces
+open Microsoft.Research.Dkal.Substrate
+
+let sdoc = @"
+<root>
+  <trials>
+    <trial1 org='org1'>
+      <site1 unnotified='true' n1='1100' n2='1250'>
+        <phys1 P1='1150' P2='1199'/>
+        <phys2 P1='1200' P2='1250'/>
+      </site1>
+    </trial1>
+    <trial2 org='org2' />
+    <trial3 org='org1' >
+      <site2 unnotified='false' n1='1300' n2='1450' />
+      <site3 unnotified='true' n1='1500' n2='1550' />
+    </trial3>
+  </trials>
+</root>"
+let xdoc = XDocument.Load(new StringReader(sdoc))
+
+let xmlsubstr = new XmlSubstrate(xdoc, ["xct"]) :> ISubstrate
+
+let output (vars: seq<ITerm>) (res: seq<ISubstitution>) =
+  res |> Seq.map (fun subst ->
+    vars |> Seq.map (fun var ->
+      match var with
+      | :? IVar as var -> var.Name+"="+(subst.Apply var).ToString()
+      | _ -> "")
+    |> String.concat " "
+  ) |> String.concat "; " // |> printfn "%A"
+
+let shouldEqual a b = if a <> b then failwith (sprintf "expected: %A, actual: %A" a b) else "Ok"
+let mapVals m = m |> Map.toList |> List.unzip |> snd
+
+let var name typ = {Name=name; Type=typ} :> IVar
+let constt c = Const(Constant c)
+
+let trialvar = var "TRIAL" Type.String
+let orgvar = var "ORG" Type.Principal
+let sitevar = var "SITE" Type.Principal
+let n1var = var "N1" Type.Int32
+let n2var = var "N2" Type.Int32
+
+
+
+/// tests
+let query1 = new XmlSubstrateQueryTerm("xct", "/root/trials/*[@org='$ORG']", [orgvar], Map.ofList ["", trialvar :> ITerm])
+
+let subst1 = Substitution.Id.Extend(orgvar, Constant "org1")
+xmlsubstr.Solve [query1] [subst1] |> output [trialvar]
+    |> shouldEqual "TRIAL=\"trial1\"; TRIAL=\"trial3\""
+
+
+let outputvars2 = Map.ofList ["", sitevar :> ITerm;  "n1", n1var :> ITerm;  "n2", n2var :> ITerm]
+let query2 = new XmlSubstrateQueryTerm("xct", "/root/trials/$TRIAL/*[@unnotified='true']", [trialvar], outputvars2)
+let subst2 = Substitution.Id.Extend(trialvar, Constant "trial1")
+xmlsubstr.Solve [query2] [subst2] |> output (mapVals outputvars2)
+    |> shouldEqual "SITE=site1 N1=1100 N2=1250"
+
+xmlsubstr.Solve [query1; query2] [subst1] |> output (Seq.append [trialvar] (mapVals outputvars2))
+    |> shouldEqual "TRIAL=\"trial1\" SITE=site1 N1=1100 N2=1250; TRIAL=\"trial3\" SITE=site3 N1=1500 N2=1550"
+
+let physvar = var "PHYS" Type.Principal
+let query3 = new XmlSubstrateQueryTerm("xct", "/root/trials/$TRIAL/$SITE/*[1100<=@P1 and @P2<=1249]", [trialvar; sitevar], Map.ofList ["", physvar :> ITerm])
+
+xmlsubstr.Solve [query1; query2; query3] [subst1] |> output (Seq.append [physvar] (outputvars2 |> Map.toList |> List.unzip |> snd))
+    |> shouldEqual "PHYS=phys1 SITE=site1 N1=1100 N2=1250"
+
+// get unnotified sites
+let getUnnotified = XmlSubstrateQueryTerm("xct", "/root/trials/*/*[@unnotified='true']", [], Map.ofList ["", sitevar :> ITerm])
+xmlsubstr.Solve [getUnnotified] [Substitution.Id] |> output [sitevar] 
+    |> shouldEqual "SITE=site1; SITE=site3"
+
+// mark all sites as unnotified
+let markUnnotified = XmlSubstrateModifyTerm("xct", "/root/trials/*/*", [], Map.ofSeq ["unnotified", Some(constt "true")]) :> ISubstrateUpdateTerm
+xmlsubstr.Update [markUnnotified]
+
+// check all sites is unnotifed
+xmlsubstr.Solve [getUnnotified] [Substitution.Id] |> output [sitevar] 
+    |> shouldEqual "SITE=site1; SITE=site2; SITE=site3"
+
+// delete test
+let deleteSite = XmlSubstrateModifyTerm("xct", "/root/trials/trial3/site2", [], Map.ofSeq ["", None])
+xmlsubstr.Update [deleteSite]
+xmlsubstr.Solve [XmlSubstrateQueryTerm("xct", "/root/trials/*/*", [], Map.ofList ["", sitevar :> ITerm])] [Substitution.Id] |> output [sitevar]
+    |> shouldEqual "SITE=site1; SITE=site3"
+
+// insert test
+let insertSite = XmlSubstrateInsertTerm("xct", "/root/trials/trial3", [], Map.ofSeq ["",constt "site2"; "unnotified", constt "false"; "n1", constt 1301; "n2", constt 1451])
+xmlsubstr.Update [insertSite]
+xmlsubstr.Solve [XmlSubstrateQueryTerm("xct", "//site2[@unnotified='false']", [], Map.ofList ["n1",n1var:>ITerm; "n2",n2var:>ITerm])] [Substitution.Id] |> output [n1var; n2var]
+    |> shouldEqual "N1=1301 N2=1451"
