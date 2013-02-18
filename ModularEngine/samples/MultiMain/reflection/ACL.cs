9b0d0a72b946fa4f2024bb824b08bbbb815dbd2a
Artem Melentyev <artem@melentyev.me>
2013-02-18 19:25:56 +0600

    add top-down permit defenition to acl example

--- ModularEngine/samples/MultiMain/reflection/ACL.cs
+++ ModularEngine/samples/MultiMain/reflection/ACL.cs
@@ -1,21 +1,63 @@
-﻿public class ACL
+﻿using System.Linq;
+using System.Collections.Generic;
+public class ACL
 {
+	private static Dictionary<string, string> obj2group = new Dictionary<string,string>()
+	{
+		/*
+		          i1,g1
+		        /       \
+		  i21,g2          i22
+		 /     \         /    \
+		i31     i32    i33,g3  i34
+		 */
+		{"i1", "g1"}, 
+		{"i1/i21", "g2"},
+		{"i1/i21/i31", null},
+		{"i1/i21/i32", null},
+
+		{"i1/i22", null},
+		{"i1/i22/i33", "g3"},
+		{"i1/i22/i34", null},
+	};
+	
 	public static string getParent(string o)
 	{
 		int i = o.LastIndexOf('/');
-		if (i < 0) return null;
-		return o.Substring(0, i);
+		return i<0 ? null : o.Substring(0, i);
 	}
 	public static bool notOwned(string o)
 	{
 		return owns(o) == null;
 	}
-	public static string[] owns(string o)
+	public static string owns(string o)
 	{
-		switch (o) {
-			case "i1": return new string[] { "g1" };
-			case "i1/i2/i3": return new string[] { "g3" };
-			default: return null;
-		}
+		return obj2group.ContainsKey(o) ? obj2group[o] : null;
+	}
+
+	private static ILookup<string, string> children = obj2group.ToLookup(t => getParent(t.Key), t => t.Key);
+	public static IEnumerable<string> getChildren(string o)
+	{
+		return children.Contains(o) ? children[o] : Enumerable.Empty<string>();
+	}
+	private static ILookup<string, string> group2obj = obj2group.ToLookup(t => t.Value, t => t.Key);
+	public static IEnumerable<string> getObjectsIOwn(string g)
+	{
+		return group2obj.Contains(g) ? group2obj[g] : Enumerable.Empty<string>();
+	}
+
+	public static IEnumerable<string> getPermittedObjects(string g)
+	{
+		foreach (string o in getObjectsIOwn(g))
+			foreach (string r in getPermittedObjectFrom(g, o))
+				yield return r;
+	}
+	private static IEnumerable<string> getPermittedObjectFrom(string g, string o)
+	{
+		yield return o;
+		foreach (string c in getChildren(o))
+			if (notOwned(c))
+				foreach (string r in getPermittedObjectFrom(g, c))
+					yield return r;
 	}
 }
